bed1278058105d9ce19296ca51191b1ae3f46859
Anton Kabanov <imcreativchik@gmail.com>
2024-01-18 19:41:45 +0500

    add loader file

---
+++ loader.py
@@ -0,0 +1,14 @@
+import os
+
+from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+bot = Bot(token=os.getenv('BOT_TOKEN'), parse_mode=types.ParseMode.HTML)
+
+storage = MemoryStorage()
+
+dp = Dispatcher(bot, storage=storage)
